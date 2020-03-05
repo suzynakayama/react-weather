8e0febc76228e10981d5e64b8498723f53ff6959
Suzy <suzy.nakayama@gmail.com>
2020-03-05 15:39:25 -0500

    Updates

---
+++ precache-manifest.fd2576a4e492e3fbec95e1eece4398a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fb0dec85aca5bfdec11ee33fbfb40d",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "8fe1a43eedbad6343934",
+    "url": "/react-weather/static/css/main.5ecadadf.chunk.css"
+  },
+  {
+    "revision": "22d5a5d396dabef7a66c",
+    "url": "/react-weather/static/js/2.86b4703f.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-weather/static/js/2.86b4703f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe1a43eedbad6343934",
+    "url": "/react-weather/static/js/main.60df100e.chunk.js"
+  },
+  {
+    "revision": "e6655350f1af38a72468",
+    "url": "/react-weather/static/js/runtime-main.1d1be39f.js"
+  }
+]);
